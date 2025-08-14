d4af0bcc24e1845d17f79ffa9d19b9579a782914
Ole Kodehode <ole.kodehode@gmail.com>
2025-08-14 13:30:35 +0200

    Changes to logic

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const playArea = document.getElementById("play-area");
+const playAreaRect = playArea.getBoundingClientRect();
 
 const player = document.createElement("div");
 player.id = "player";
-player.style.left = "500px";
-player.style.top = "500px";
+player.style.left = "50px";
+player.style.top = "50px";
 
 playArea.append(player);
 
@@ -33,15 +34,33 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
-document.addEventListener("click", () => {
-  console.log("click detected");
+playArea.addEventListener("click", (e) => {
+  console.log(
+    `Click detected at ${(e.clientX - playAreaRect.left).toFixed(0)} / ${(
+      e.clientY - playAreaRect.top
+    ).toFixed(0)}`
+  );
 });
 
 function movePlayer(addY, addX) {
   let yPos = Number(player.style.top.replace("px", ""));
   let xPos = Number(player.style.left.replace("px", ""));
-  yPos = yPos <= 0 ? 0 : (yPos += addY);
-  xPos = xPos <= 0 ? 0 : (xPos += addX);
+  const max_X = Math.round(playAreaRect.width - 32);
+  const max_Y = Math.round(playAreaRect.height - 32);
+  console.log(max_X, max_Y);
+  if (yPos + addY > max_Y) {
+    return;
+  } else if (yPos + addY < 0) {
+    return;
+  }
+  if (xPos + addX > max_X) {
+    return;
+  } else if (xPos + addX < 0) {
+    return;
+  }
+
+  yPos += addY;
+  xPos += addX;
   player.style.top = `${yPos}px`;
   player.style.left = `${xPos}px`;
 }
